01902662fb99584319fce6c7e577135b2b640c50
davecandawan <dave@sicurobrands.com>
2025-07-23 21:36:34 +0800

    Update page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,29 +4,29 @@ import '@/styles/globals.css';
 import '@/styles/fonts.css';
 
 export const metadata: Metadata = {
-  title: 'VNSH Laser Strike System + FREE Holster!',
-  description: 'Laser Strike System + FREE Holster!',
+  title: 'VNSH Laser Strike System + FREE Camo Holster!',
+  description: 'Laser Strike System + FREE Camo Holster!',
   icons: {
     icon: '/favicon.png',
     apple: '/favicon.png',
   },
   openGraph: {
-    title: 'VNSH Laser Strike System + FREE Holster!',
-    description: 'Laser Strike System + FREE Holster!',
+    title: 'VNSH Laser Strike System + FREE Camo Holster!',
+    description: 'Laser Strike System + FREE Camo Holster!',
     type: 'website',
     images: [
       {
         url: '/favicon.png',
         width: 512,
         height: 512,
-        alt: 'VNSH Laser Strike System + FREE Holster!',
+        alt: 'VNSH Laser Strike System + FREE Camo Holster!',
       },
     ],
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'VNSH Laser Strike System + FREE Holster!',
-    description: 'Laser Strike System + FREE Holster!',
+    title: 'VNSH Laser Strike System + FREE Camo Holster!',
+    description: 'Laser Strike System + FREE Camo Holster!',
     images: ['/favicon.png'],
   },
 };
